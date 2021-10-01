25644bb105ed47b67ee903a9b1bda2dba53159a9
Visethboti <visethboti@gmail.com>
2021-09-30 23:29:23 -0500

    Starting out

---
+++ Main.java
@@ -0,0 +1,34 @@
+
+
+
+
+public class Main {
+	public static void main(String[] args){  
+
+		//System.out.println("hello world");
+	}
+
+	private static void initialization(){
+	  
+	}
+	
+	private static void fitnessCalculation(){
+		
+	}
+
+	private static void parentSelection(){
+	  
+	}
+
+	private static void crossOver(){
+	  
+	}
+	
+	private static void replaceGeneration(){
+		
+	}
+
+	private static void mutation(){
+	  
+	}
+}
